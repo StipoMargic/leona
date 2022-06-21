325122f16989e4d0b500bb77451816e31f0e1861
Stipo Margić <stipo@udruga-liberato.hr>
2022-06-21 20:12:38 +0200

    routes

--- config/jwt.js
+++ config/jwt.js
@@ -1,25 +1,24 @@
 const jwt = require("jsonwebtoken");
 
 function signJwt(user_id, role) {
-    const token = jwt.sign({sub: user_id, role}, "leona"); 
-    if (!token) return false;
-    return token;
+	const token = jwt.sign({ sub: user_id, role }, "leona");
+	if (!token) return false;
+	return token;
 }
 
 function verifyJwt(req, res, next) {
-    const authorization = req.header('authorization');
-    const token = authorization ? authorization.split('Bearer ')[1] : undefined;
-    if(!token) {
-        return res.send(401, "Unauthorized");
-    }
-    jwt.verify(token, "leona", (err, payload)=>{
-        if (err || !payload.sub) {
-            return res.send(401, "Unauthorized");
-        }
-        console.log(payload)
-        req.userId = payload;
-        return next();
-    })
+	const authorization = req.header("authorization");
+	const token = authorization ? authorization.split("Bearer ")[1] : undefined;
+	if (!token) {
+		return res.send(401, "Unauthorized");
+	}
+	jwt.verify(token, "leona", (err, payload) => {
+		if (err || !payload.sub) {
+			return res.send(401, "Unauthorized");
+		}
+		req.user = payload;
+		return next();
+	});
 }
 
 module.exports = { signJwt, verifyJwt };
